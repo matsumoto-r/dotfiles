25e4a4815ecc63af2dc40132314990e7bf8c318e
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-04-14 19:57:15 +0900

    Add some package installer for ubuntu

---
+++ ubuntu-apt-get.sh
@@ -0,0 +1,3 @@
+sudo apt-get -y update
+
+sudo apt-get -y install rake bison git gperf libmemcached-dev libcurl4-openssl-dev libhiredis-dev libmarkdown2-dev libcgroup-dev libcap-dev libjansson-dev redis-server iproute whois apache2 apache2-dev rake bison libcurl4-openssl-dev libhiredis-dev libmarkdown2-dev liblua5.1-0-dev vim screen memcached
