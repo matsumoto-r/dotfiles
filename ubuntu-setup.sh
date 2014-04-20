4dff4d19db7dd78f5c75258400cb0047b6f7160a
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-04-20 03:14:12 -0700

    Add ubuntu-setup script

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,5 +1,11 @@
-sudo apt-get -y update
+cp -pr .bashrc ~/.
+cp -pr .gitconfig ~/.
+cp -pr .screenrc ~/.
+cp -pr .ssh ~/.
+cp -pr .vim ~/.
+cp -pr .vimrc ~/.
 
+sudo apt-get -y update
 sudo apt-get -y install rake bison git gperf libmemcached-dev libcurl4-openssl-dev libhiredis-dev libmarkdown2-dev libcgroup-dev libcap-dev libjansson-dev redis-server iproute whois apache2 apache2-dev rake bison libcurl4-openssl-dev libhiredis-dev libmarkdown2-dev liblua5.1-0-dev vim screen memcached
 sudo apt-get install -y autoconf
 sudo apt-get install -y libprotobuf-c0-dev
