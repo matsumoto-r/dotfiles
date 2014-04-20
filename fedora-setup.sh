8cdf7d47a7eb0e5b3421388baaa22f5bfcf66ef1
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-04-21 01:04:31 +0900

    Add fedora setup script

---
+++ fedora-setup.sh
@@ -0,0 +1,18 @@
+cp -pvfr .bashrc ~/.
+cp -pvfr .gitconfig ~/.
+cp -pvfr .screenrc ~/.
+cp -pvfr .ssh ~/.
+cp -pvfr .vim ~/.
+cp -pvfr .vimrc ~/.
+
+# TODO: ADD yum install
+#sudo apt-get -y update
+#sudo apt-get -y install rake bison git gperf libmemcached-dev libcurl4-openssl-dev libhiredis-dev libmarkdown2-dev libcgroup-dev libcap-dev libjansson-dev redis-server iproute whois apache2 apache2-dev rake bison libcurl4-openssl-dev libhiredis-dev libmarkdown2-dev liblua5.1-0-dev vim screen memcached
+#sudo apt-get install -y autoconf
+#sudo apt-get install -y libprotobuf-c0-dev
+#sudo apt-get install -y libprotoc-dev
+#sudo apt-get install -y protobuf-c-compiler
+#sudo apt-get install -y asciidoc
+#sudo apt-get install -y libxml2-dev libevent-dev libjemalloc-dev libjansson-dev
+#sudo apt-get install -y libcunit1-dev
+
