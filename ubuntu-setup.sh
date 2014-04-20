1db1a09c8e2c8a42ba59d2bd94c725b0c933035e
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-04-20 03:16:11 -0700

    Add verbose option

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,9 +1,9 @@
-cp -pr .bashrc ~/.
-cp -pr .gitconfig ~/.
-cp -pr .screenrc ~/.
-cp -pr .ssh ~/.
-cp -pr .vim ~/.
-cp -pr .vimrc ~/.
+cp -pvfr .bashrc ~/.
+cp -pvfr .gitconfig ~/.
+cp -pvfr .screenrc ~/.
+cp -pvfr .ssh ~/.
+cp -pvfr .vim ~/.
+cp -pvfr .vimrc ~/.
 
 sudo apt-get -y update
 sudo apt-get -y install rake bison git gperf libmemcached-dev libcurl4-openssl-dev libhiredis-dev libmarkdown2-dev libcgroup-dev libcap-dev libjansson-dev redis-server iproute whois apache2 apache2-dev rake bison libcurl4-openssl-dev libhiredis-dev libmarkdown2-dev liblua5.1-0-dev vim screen memcached
