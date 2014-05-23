9c5588b8656a50fc56a9941dd4d4ee4658f76f52
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-05-23 09:15:05 -0700

    Add kvm setup

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -11,11 +11,11 @@ cp -pvfr .vimrc ~/.
 
 # install packages
 sudo apt-get -y update
-sudo apt-get install -y rake bison git gperf libmemcached-dev 
-sudo apt-get install -y libcurl4-openssl-dev libhiredis-dev libmarkdown2-dev 
-sudo apt-get install -y libcgroup-dev libcap-dev libjansson-dev redis-server 
-sudo apt-get install -y iproute whois apache2 apache2-dev rake bison 
-sudo apt-get install -y libcurl4-openssl-dev libhiredis-dev libmarkdown2-dev 
+sudo apt-get install -y rake bison git gperf libmemcached-dev
+sudo apt-get install -y libcurl4-openssl-dev libhiredis-dev libmarkdown2-dev
+sudo apt-get install -y libcgroup-dev libcap-dev libjansson-dev redis-server
+sudo apt-get install -y iproute whois apache2 apache2-dev rake bison
+sudo apt-get install -y libcurl4-openssl-dev libhiredis-dev libmarkdown2-dev
 sudo apt-get install -y liblua5.1-0-dev vim screen memcached
 sudo apt-get install -y autoconf valgrind
 sudo apt-get install -y libprotobuf-c0-dev
@@ -25,16 +25,20 @@ sudo apt-get install -y asciidoc
 sudo apt-get install -y libxml2-dev libevent-dev libjemalloc-dev libjansson-dev
 sudo apt-get install -y libcunit1-dev
 sudo apt-get install -y openjdk-7-jdk autotools-dev libltdl-dev libtool autoconf
-sudo apt-get install -y autopoint libboost-all-dev genromfs zfs-fuse autoconf 
-sudo apt-get install -y ant libffi-dev bison gyp git build-essential qemu-utils 
+sudo apt-get install -y autopoint libboost-all-dev genromfs zfs-fuse autoconf
+sudo apt-get install -y ant libffi-dev bison gyp git build-essential qemu-utils
 sudo apt-get install -y qemu-system-x86 bridge-utils libvirt0 libvirt-bin
 sudo apt-get install -y apache2-utils
 sudo apt-get install -y build-essential libboost-all-dev genromfs autoconf
-sudo apt-get install -y maven 
+sudo apt-get install -y maven
 sudo apt-get install -y libmaven-shade-plugin-java
 sudo apt-get install -y python-dpkt tcpdump gdb tcl-dev
 sudo apt-get install -y ruby2.0 ruby2.0-dev
 
 # install go
 wget https://storage.googleapis.com/golang/go1.2.2.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.2.2.linux-amd64.tar.gz 
+tar -C /usr/local -xzf go1.2.2.linux-amd64.tar.gz
+
+# setup kvm
+sudo /sbin/modprobe kvm
+sudo gpasswd -a matsumotory kvm
