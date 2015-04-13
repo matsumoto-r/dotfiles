eaa0e4454474aba41ff711043560cdbca589c5fa
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-04-13 23:22:13 +0900

    Add centos setup script

--- fedora-centos-setup.sh
+++ fedora-centos-setup.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 cp -pvfr .bashrc ~/.
 cp -pvfr .gitconfig ~/.
 cp -pvfr .screenrc ~/.
@@ -16,3 +18,25 @@ cp -pvfr .vimrc ~/.
 #sudo apt-get install -y libxml2-dev libevent-dev libjemalloc-dev libjansson-dev
 #sudo apt-get install -y libcunit1-dev
 
+yum -y groupinstall "Development Tools"
+yum -y install bison
+yum -y install openssl-devel
+yum -y install pcre-devel
+yum -y install tar
+yum -y install wget
+yum -y install zlib-devel
+
+rpm --import http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6
+rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+rpm --import http://rpms.famillecollet.com/RPM-GPG-KEY-remi
+rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+yum -y install --enablerepo=remi libmemcached-last-devel
+
+cd /usr/local/src/
+git clone https://github.com/sstephenson/ruby-build.git
+
+cd /usr/local/src/ruby-build
+sh install.sh
+ruby-build 2.1.4 /usr/local/ruby-2.1.4
+export PATH=/usr/local/ruby-2.1.4/bin:$PATH
+cd ~/
